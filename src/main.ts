7883142b2e962243f11c1e1c992f2998ff77d5fb
Balestrino <salvatore.balestrino@gmail.com>
2025-01-29 11:35:30 +0000

    Add class-transformer and class-validator packages; implement global validation pipe in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Removes any properties from the incoming request body that are not defined in the DTO. This helps prevent overposting attacks
+      transform: true, // Automatically transforms incoming data to match the types defined in the DTO
+      transformOptions: {
+        enableImplicitConversion: true, // Enables implicit type conversions, such as converting strings to numbers or booleans
+      },
+      forbidNonWhitelisted: true, // Forbid extra properties
+      forbidUnknownValues: true, // Forbid unknown values in the incoming request body
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
